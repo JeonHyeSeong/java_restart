ce834cbdd3358bc4f71591a9112c6954e340db8d
DESKTOP-LNPBGI2\bc739 <bc7399@naver.com>
2024-01-04 15:02:38 +0900

    자바 복습 / 변수,연산자,if문,switch문

---
+++ java-start/src/operator/Operator1.java
@@ -0,0 +1,25 @@
+package operator;
+
+public class Operator1 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+
+        int sum = a + b;
+        System.out.println("a + b = "+sum);
+
+        int diff = a - b;
+        System.out.println("a - b = " + diff);
+
+        int mul = a * b;
+        System.out.println("a * b = "+mul);
+
+        int div = a / b;
+        System.out.println("a / b = " + div);
+
+        int mod = a % b;
+        System.out.println("a % b = "+ mod);
+
+        //int z = 10 / 0; // 숫자는 0으로 나눌 수 없음
+    }
+}
